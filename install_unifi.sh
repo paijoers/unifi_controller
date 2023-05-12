5d09d7784c70261f0ba61c6ae66b66bc200d44ac
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-13 01:02:26 +0700

    Update install_unifi.sh

--- install_unifi.sh
+++ install_unifi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Tiktok konten @nys.pjr 
+
 # Ubiquiti Repository
 echo "deb http://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
