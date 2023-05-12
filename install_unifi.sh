1ac934bca89112d69122d488d5015f2beac780c9
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-12 22:32:09 +0700

    Add files via upload

---
+++ install_unifi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Tiktok konten @nys.pjr 
+# Ubiquiti Repository
+echo "deb http://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
+
+# Memperbarui paket sistem
+sudo apt update
+sudo apt upgrade -y
+
+# Menginstal rng-tools
+sudo apt install -y rng-tools
+sudo sed -i 's/#HRNGDEVICE=/dev/hwrng/HRNGDEVICE=/dev/urandom/' /etc/default/rng-tools
+sudo systemctl restart rng-tools
+
+# Menginstal Unifi Controller
+sudo apt install -y unifi
