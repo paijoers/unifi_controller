eec2c1a683130d1077e741ec69de6fa694be03de
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-13 20:55:36 +0700

    Update install_unifi.sh

--- install_unifi.sh
+++ install_unifi.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 # @nys.pjr 
 
+# Menginstal rng-tools
+sudo apt-get install -y rng-tools
+sudo touch /etc/default/rng-tools
+sudo sed -i 's/#HRNGDEVICE=\/dev\/hwrng/HRNGDEVICE=\/dev\/urandom/' /etc/default/rng-tools
+sudo systemctl restart rng-tools
+# sudo /etc/init.d/rng-tools restart
+
+# Menghapus haveged
+sudo systemctl stop haveged
+sudo apt-get purge haveged -y
+sudo apt-get autoremove -y
+
 # Ubiquiti Repository
 echo "deb http://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 
 sudo apt-get update
 
-# Menginstal rng-tools
-sudo apt-get install -y rng-tools
-sudo touch /etc/default/rng-tools
-sudo sed -i 's/#HRNGDEVICE=\/dev\/hwrng/HRNGDEVICE=\/dev\/urandom/' /etc/default/rng-tools
-# sudo systemctl restart rng-tools
-sudo /etc/init.d/rng-tools restart
 
-# Menghapus haveged
-sudo systemctl stop haveged
-sudo apt-get purge haveged -y
-sudo apt autoremove -y
 
 # Menginstal Unifi Controller
 sudo apt-get install -y unifi
