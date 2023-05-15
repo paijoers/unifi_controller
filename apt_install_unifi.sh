e75e284e4bcc437ea586cbe542b6c7cb02f0732c
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-16 01:06:26 +0700

    Update apt_install_unifi.sh

--- apt_install_unifi.sh
+++ apt_install_unifi.sh
@@ -1,25 +1,65 @@
 #!/bin/bash
+# @nys.pjr 
+# Tested OS: Armbian 20.10 Ubuntu Bionic
 
-# Menambahkan repositori UniFi
-echo 'deb http://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
+# Set Java path
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
 
-# Menambahkan kunci GPG untuk repositori
-sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
-
-# Menambahkan repositori MongoDb
-wget -qO - https://www.mongodb.org/static/pgp/server-3.6.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-
-# Memperbarui daftar paket
+# Update packages
 sudo apt update
 
-# Menginstall Java 11
-sudo apt install openjdk-11-jdk -y
+# Install rng-tools
+sudo apt install -y rng-tools
+sudo touch /etc/default/rng-tools
+sudo sed -i 's|^#HRNGDEVICE=.*|HRNGDEVICE=/dev/urandom|' /etc/default/rng-tools
+sudo systemctl restart rng-tools
 
-# Menginstall MongoDB
-sudo apt install mongodb-org -y
+# Disable haveged
+sudo systemctl stop haveged
+sudo systemctl disable haveged
 
-# Menginstal UniFi Controller
-sudo apt install unifi -y
+# Option to install UniFi Controller via apt
+read -p "Do you want to install UniFi Controller via apt? (y/n): " choice
+if [[ $choice =~ ^[Yy]$ ]]; then
+    # Add UniFi repository key
+    wget -O - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+    echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+    
+    # Add UniFi repository
+    echo "deb https://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/unifi.list
+    
+    # Update packages again
+    sudo apt update
+    
+    # Install dependencies
+    sudo apt install -y openjdk-11-jre-headless mongodb
 
-# Done!
+    # Install UniFi Controller
+    sudo apt install -y unifi
+else
+    # Check if unifi_sysvinit_all.deb file exists
+    if [ -f unifi_sysvinit_all.deb ]; then
+        rm unifi_sysvinit_all.deb
+    fi
+
+    # Download UniFi Controller version 7.3.83
+    wget -c https://dl.ui.com/unifi/7.3.83/unifi_sysvinit_all.deb
+
+    # Install dependencies
+    sudo apt install -y openjdk-11-jre-headless mongodb
+
+    # Install UniFi Controller from downloaded package
+    sudo dpkg -i unifi_sysvinit_all.deb
+
+    # Install dependencies
+    sudo apt install -f -y
+fi
+
+# Start UniFi Controller
+sudo systemctl start unifi
+
+# Enable UniFi Controller to start on boot
+sudo systemctl enable unifi
+
+echo "UniFi Controller has been installed and started."
