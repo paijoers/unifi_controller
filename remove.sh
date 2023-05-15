3b9671f31f6530a4f6350eaea1b1ed236354450f
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-16 01:34:28 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -17,11 +17,17 @@ sudo apt purge openjdk-11-* -y
 # Uninstall MongoDB with purge
 sudo apt purge mongodb-org* -y
 
-# Uninstall rng-tools with purge
-sudo apt purge rng-tools -y
+# Remove UniFi source list
+sudo rm /etc/apt/sources.list.d/unifi.list
+
+# Remove MongoDB source list
+sudo rm /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+# Remove unused packages
+sudo apt autoremove -y
 
 # Re-enable haveged
 sudo systemctl start haveged
 sudo systemctl enable haveged
 
-echo "UniFi Controller and related packages have been completely uninstalled."
+echo "UniFi Controller, related packages, and source lists have been completely uninstalled."
