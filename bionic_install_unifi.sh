613e1bbf50e408f89d4b915468ae2bf479f4cc0f
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-16 00:38:19 +0700

    Update bionic_install_unifi.sh

--- bionic_install_unifi.sh
+++ bionic_install_unifi.sh
@@ -14,25 +14,27 @@ sudo apt update
 # Menginstal rng-tools
 sudo apt install -y rng-tools
 sudo touch /etc/default/rng-tools
-sudo sed -i 's/#HRNGDEVICE=\/dev\/hwrng/HRNGDEVICE=\/dev\/urandom/' /etc/default/rng-tools
+sudo sed -i 's|^#HRNGDEVICE=.*|HRNGDEVICE=/dev/urandom|' /etc/default/rng-tools
 sudo systemctl restart rng-tools
-# sudo /etc/init.d/rng-tools restart
 
 # Disable haveged
 sudo systemctl stop haveged
 sudo systemctl disable haveged
 
 # Memasang dependensi yang diperlukan
-sudo apt install -y openjdk-8-jre-headless mongodb
+sudo apt install -y openjdk-11-jre-headless mongodb
 
 # Mengunduh UniFi Controller
-wget -c https://dl.ui.com/unifi/7.2.95/unifi_sysvinit_all.deb
+wget -c https://dl.ui.com/unifi/7.3.83/unifi_sysvinit_all.deb
 
 # Memasang UniFi Controller
 sudo dpkg -i unifi_sysvinit_all.deb
 
 # Menginstal Unifi Controller
 sudo apt install unifi -y
+
+# Menjalankan UniFi Controller
+sudo systemctl start unifi
 
 # Mengatur UniFi Controller untuk memulai pada saat boot
 sudo systemctl enable unifi
