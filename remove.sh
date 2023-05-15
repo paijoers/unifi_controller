3d06bc3b5ae76bec9f4059267ad9b15d2121005a
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-16 01:19:49 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -8,20 +8,20 @@ sudo systemctl stop unifi
 # Disable UniFi Controller from starting on boot
 sudo systemctl disable unifi
 
-# Uninstall UniFi Controller
-sudo apt remove unifi -y
+# Uninstall UniFi Controller with purge
+sudo apt purge unifi -y
 
-# Uninstall OpenJDK 11
-sudo apt remove openjdk-11-* -y
+# Uninstall OpenJDK 11 with purge
+sudo apt purge openjdk-11-* -y
 
-# Uninstall MongoDB
-sudo apt remove mongodb-org* -y
+# Uninstall MongoDB with purge
+sudo apt purge mongodb-org* -y
 
-# Uninstall rng-tools
-sudo apt remove rng-tools -y
+# Uninstall rng-tools with purge
+sudo apt purge rng-tools -y
 
 # Re-enable haveged
 sudo systemctl start haveged
 sudo systemctl enable haveged
 
-echo "UniFi Controller and related packages have been uninstalled."
+echo "UniFi Controller and related packages have been completely uninstalled."
