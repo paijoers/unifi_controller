1ded2161c0385adfa6298723299d14819395c615
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-21 20:41:26 +0700

    Update armbian_unifi_controller.sh

--- armbian_unifi_controller.sh
+++ armbian_unifi_controller.sh
@@ -109,8 +109,8 @@ cleanup_unifi() {
 }
 
 # Show menu and ask for user input
-echo "UniFi Controller Installation"
-echo -e "\n---------------------------\n"
+echo -e "UniFi Controller Installation"
+echo -e "---------------------------\n"
 echo "1. Install UniFi Controller via apt"
 echo "2. Manual Install (Install UniFi Controller without using apt)"
 echo "3. Clean Up (Remove installed UniFi Controller packages)"
