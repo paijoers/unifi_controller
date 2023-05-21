bb0d42c829aba134732a05c44433d985254dfeae
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-21 18:54:11 +0700

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -84,6 +84,7 @@ cleanup_unifi() {
     sudo systemctl start haveged
     sudo apt purge -y rng-tools
     sudo rm /etc/default/rng-tools
+    sudo apt autoremove
     echo "UniFi Controller has been successfully removed."
     exit 0
 }
