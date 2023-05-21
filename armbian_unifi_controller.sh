9e8689c93a18da767479919ebe991c5c425ca9bd
paijoers <74069017+paijoers@users.noreply.github.com>
2023-05-21 20:53:13 +0700

    Update armbian_unifi_controller.sh

--- armbian_unifi_controller.sh
+++ armbian_unifi_controller.sh
@@ -56,7 +56,7 @@ install_unifi_manual() {
         wget -O - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
         echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
         sudo apt update
-        sudo apt install -y mongodb-org
+        sudo apt install -y mongodb
 
         # Install UniFi Controller
         sudo dpkg -i unifi_sysvinit_all.deb
